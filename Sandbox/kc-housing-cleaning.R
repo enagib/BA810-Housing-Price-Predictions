0214c75ba349af569c8d83fb0968d4cbe1dcc5ff
Eman <enagib@bu.edu>
2019-10-07 13:50:17 +0000

    rm yr_renovated and added dummy variable for renovated

--- Sandbox/kc-housing-cleaning.R
+++ Sandbox/kc-housing-cleaning.R
@@ -1,8 +1,14 @@
 install.packages("tidyverse")
 library(tidyverse)
+library(ggplot2)
 
 ## Loading our table
 kc_housing <- read_csv("kc_house_data.csv")
+
+## we viewd the id and found that some id are not unique this is because some houses are
+## sold twice so we group_by id, and date to see if they are redudant, and it turned out that
+## every transaction is unique
+kc_housing %>% group_by(id, date) %>% summarise(count = n()) %>% filter(count>1)
 
 ## Examining table
 View(kc_housing)
@@ -35,11 +41,26 @@ nrow(kc_houses)
 nrow(kc_housing)
 summary(kc_housing)
 
-## we viewd the id and found that some id are not unique this is because some houses are
-## sold twice so we group_by id, and date to see if they are redudant, and it turned out that
-## every transaction is unique
-kc_housing %>% group_by(id, date) %>% summarise(count = n()) %>% filter(count>1)
-
-
 ## save table as csv
 write_csv(kc_houses, "kc_houses_table.csv")
+
+## most expensive house
+kc_houses %>%  filter(price == max(price)) %>%View()
+
+ggplot(kc_housing, aes(bedrooms, price)) + 
+  geom_point(positon = "jitter")+
+  scale_y_continuous(breaks = seq(0,max(kc_housing$price),1000000))
+
+options(scipen = 999)
+ggplot(kc_housing, aes(sqft_living, price, col = view)) + 
+  geom_point(alpha = 0.3) +
+  scale_y_continuous(breaks = seq(0,max(kc_housing$price),500000))
+
+View(kc_houses)
+
+kc_houses <- kc_houses %>% 
+  mutate(renovated = ifelse(yr_renovated >0, 1, 0)) %>% 
+  select(everything(), -yr_renovated)
+  
+View(kc_houses)
+
